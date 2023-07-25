0b426c10a77b2465dade60613bc4b78fba16c923
kresohr <48351282+kresohr@users.noreply.github.com>
2023-07-25 16:43:53 +0200

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../assets/styles/HeaderStyle.scss'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className='header__inner'>
+            <div>Inner</div>
+            <div>Inner</div>
+            <div>Inner</div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assets/styles/HomepageStyle.scss'
+
+function Homepage() {
+  return (
+    <div>Homepage</div>
+  )
+}
+
+export default Homepage
